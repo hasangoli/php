c5f8ee87016399c8b3e8e849e6e28c0a3c7a2855
hasangoli <mrhasangoli@gmail.com>
2022-04-20 13:58:02 +0430

    initial

--- 11_sanitizing_inputs.php
+++ 11_sanitizing_inputs.php
@@ -4,3 +4,26 @@
 /*
   Data submitted through a form is not sanitized by default. We have methods to sanitize data manually.
 */
+
+if (isset($_POST['submit'])) {
+  $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+  $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+
+  // $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+
+  echo $name;
+}
+
+?>
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+  <div>
+    <label for="name">Name:</label>
+    <input type="text" name="name">
+  </div>
+  <div>
+    <label for="age">Age:</label>
+    <input type="number" name="age">
+  </div>
+  <input type="submit" value="Submit" name="submit">
+</form>
