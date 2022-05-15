37f86cdc521778f994ef1ee79e49bce09f8af604
hasangoli <mrhasangoli@gmail.com>
2022-05-15 22:16:25 +0430

    initial

--- 13_sessions.php
+++ 13_sessions.php
@@ -5,31 +5,3 @@
   Sessions are a way to store information (in variables) to be used across multiple pages.
   Unlike cookies, sessions are stored on the server.
 */
-
-session_start();
-
-if (isset($_POST['submit'])) {
-  $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
-
-  $password = $_POST['password'];
-
-  if ($username == 'sann' && $password == 'pass') {
-    $_SESSION['username'] = $username;
-    header('Location: /extras/dashboard.php');
-  } else {
-    echo "Incorrect inputs";
-  }
-}
-?>
-
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-  <div>
-    <label for="username">Username:</label>
-    <input type="text" name="username">
-  </div>
-  <div>
-    <label for="password">Password:</label>
-    <input type="password" name="password">
-  </div>
-  <input type="submit" value="Submit" name="submit">
-</form>
